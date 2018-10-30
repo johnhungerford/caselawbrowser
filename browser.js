7b842faab6407c9af2e3d3748b3a1d4bfa207de3
johnhungerford <hungerfordjustice@gmail.com>
2018-10-30 14:24:35 -0400

    just started

---
+++ browser.js
@@ -0,0 +1,20 @@
+browse function
+	State			| 		Federal
+	States					Courts/Search
+	Courts/Search			
+
+Date Bar
+Title Contains
+Text Contains
+
+
+/ : landing page
+/state : states
+/federal : federal courts
+/scotus : supreme court
+
+
+landing page: just choose between the three, but also search full text
+state: browser (can't choose court without choosing state), can't search title without choosing court
+federal: browser (no state, just court)
+scotus: browser ()

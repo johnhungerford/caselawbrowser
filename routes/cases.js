9efad9f85511cf854b8580d4ee4836742dbf1210
johnhungerford <hungerfordjustice@gmail.com>
2018-11-01 23:21:22 -0400

    Cleaned up old files

--- routes/cases.js
+++ routes/cases.js
@@ -7,25 +7,25 @@ router.get('/', function(req, res, next) {
 
 	// console.log(req.query.min + " - " + req.query.max);
 
-  if ( !req.query.url ) { 
-    console.log("No url!");
-    next(); 
-  }
+	if ( !req.query.url ) { 
+		console.log("No url!");
+		next(); 
+	}
 
 	console.log('CAP API search url: ' + req.query.url);
 
 	request.get({
-    	url: req.query.url,
-    	json: true
-  	}, (caperr, capres, capdata) => {
-    	if (caperr) {
-      		console.log('Error:', caperr);
-    	} else if (capres.statusCode !== 200) {
-      		console.log('Status:', capres);
-    	} else {
-      		// data is already parsed as JSON:
-      		res.send(capdata);
-    	}
+			url: req.query.url,
+			json: true
+		}, (caperr, capres, capdata) => {
+			if (caperr) {
+					console.log('Error:', caperr);
+			} else if (capres.statusCode !== 200) {
+					console.log('Status:', capres);
+			} else {
+					// data is already parsed as JSON:
+					res.send(capdata);
+			}
 	});
 });
 

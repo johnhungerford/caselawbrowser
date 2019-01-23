860f78bb8bafe9cbb66f0fea7adaa3929ee35caa
johnhungerford <hungerfordjustice@gmail.com>
2019-01-23 14:28:58 -0500

    fixed a couple public javascripts: fullcase view can refresh properly now, and citation searches work

--- public/javascripts/fullcase.js
+++ public/javascripts/fullcase.js
@@ -13,6 +13,11 @@ var refresh = function () {
 
 	$.getJSON( getCaseUrl, { "full_text": true, "body_format": "html" }, function (data) {
 
+		$('#case-title').empty();
+		$('#case-info').empty();
+		$('#case-text').empty();
+
+
 		if(data.casebody.data) {
 			$('#docx-box').append('<a href="/getcase/' + data.id + '/docx?key='+ $('#key-field').val()+'" id="docxlink">Download case as a word document</a>');
 		}
@@ -31,52 +36,11 @@ var refresh = function () {
 		if( data.casebody ) {
 
 			if( data.casebody.data ) {
-
 				$('#case-text').append(data.casebody.data);
-
-				/*var casedata = data.casebody.data;
-				if (casedata.parties) { 
-					var parties = casedata.parties.join(", ");
-					$('#case-info').append('<li>Parties: ' + parties + '</li>');
-				}
-				if (casedata.judges) { 
-					var judges = casedata.judges.join(", ");
-					$('#case-info').append('<li>Judges: ' + judges + '</li>');
-				}
-				if (casedata.attorneys) {
-					var attorneys = casedata.attorneys.join(", ");
-					$('#case-info').append('<li>Attorneys: ' + attorneys + '</li>');
-				}
-
-				$('#front-matter').append('<h2>' + data.name + '</h2>');
-				var frontMatterParas = casedata.head_matter.split('\n');
-				var frontMatterHTML = '';
-				for (var j = 0; j < frontMatterParas.length; j ++) {
-					frontMatterHTML += '<p class="front-matter-para">' + frontMatterParas[j] + '</p>';
-				}
-				if( casedata.head_matter ) { $('#front-matter').append( frontMatterHTML ) };
-
-				var opinions = casedata.opinions;
-
-				for( var i = 0; i < opinions.length; i++ ) {
-					$('#opinions').append($('<div>', {
-						id: 'opinion' + i,
-						class: 'opinion',
-					}));
-
-					var opinionParas = opinions[i].text.split('\n');
-					var opinionHTML = '';
-					for (var j = 0; j < opinionParas.length; j ++) {
-						opinionHTML += '<p class="opinion-para">' + opinionParas[j] + '</p>';
-					}
-					$('#opinion' + i).append('<h3>' + opinions[i].author + ': ' + opinions[i].type + '</h2>');
-					$('#opinion' + i).append( opinionHTML );
-				}*/	
-
 			} else {
-				$('#front-matter').append('<h2>You need a CAP API token to access the full case</h2>');
-				$('#front-matter').append('<p>You can obtain a token by registering with Harvard LIL. Registration is free. Each token can access up to 500 full cases per day.</p>');
-				$('#front-matter').append('<p><a href="https://case.law/user/register/">Click here to register with Harvard LIL</a></p>');
+				$('#case-text').append('<h2>You need a CAP API token to access the full case</h2>');
+				$('#case-text').append('<p>You can obtain a token by registering with Harvard LIL. Registration is free. Each token can access up to 500 full cases per day.</p>');
+				$('#case-text').append('<p><a href="https://case.law/user/register/">Click here to register with Harvard LIL</a></p>');
 			}
 
 		}
